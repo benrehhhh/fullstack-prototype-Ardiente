0de08377682abe9041b9397b662dc60eacb0ec75
Vin Henry S. Ardiente <vinhenrysaballaardiente@gmail.com>
2026-02-20 00:18:06 +0800

    Finished Phase 3: Authentication System

--- script.js
+++ script.js
@@ -70,9 +70,17 @@ function handleRouting(){
 // ==============================
 
 window.db = {
-    accounts: []
+    accounts: JSON.parse(localStorage.getItem("db_accounts")) || []
+};
+
+function saveToStorage(){
+    localStorage.setItem("db_accounts", JSON.stringify(window.db.accounts));
 }
 
+
+// =================
+//  REGISTER FORM
+// =================
 document.getElementById("register-form").addEventListener("submit", function (e){
     e.preventDefault();
 
@@ -80,6 +88,12 @@ document.getElementById("register-form").addEventListener("submit", function (e)
     const last = document.getElementById("reg-lastname").value.trim();
     const email = document.getElementById("reg-email").value.trim();
     const password = document.getElementById("reg-password").value.trim();
+
+    if(password.length <6)
+    {
+        alert("Password must be at least 6 characters.");
+        return;
+    }
 
     const existing = window.db.accounts.find(acc => acc.email === email);
     if(existing){
@@ -98,4 +112,101 @@ document.getElementById("register-form").addEventListener("submit", function (e)
 
     localStorage.setItem("unverified_email", email);
     navigateTo("#/verify-email");
+});
+
+// =====================
+//  EMAIL VERIFICATION
+// =====================
+
+document.getElementById("simulate-verfication").addEventListener("click", function(){
+    const email = localStorage.getItem("unverified_email");
+    if (!email)
+    {
+        alert("No unverified email found.");
+        return;
+    }
+
+    const targetAccount = window.db.accounts.find(acc => acc.email === email);
+    if (targetAccount){
+        targetAccount.verified = true;
+        saveToStorage();
+        alert("Email Verified! Please Login.");
+        navigateTo("#/login");
+    }
+});
+
+
+// ==================
+//   LOGIN FORM
+// ==================
+document.getElementById("login-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const email = document.getElementById("log-email").value.trim();
+    const password = document.getElementById("log-password").value.trim();
+
+    const account = window.db.accounts.find(acc => acc.email === email && acc.password === password);
+
+    if(!account)
+    {
+        alert("Email or password incorrect.");
+        return;
+    }
+
+    if(!account.verified)
+    {
+        alert("Email not verified. Please verify first. ");
+        return;
+    }
+
+    localStorage.setItem("auth_token", email);
+    setAuthState(true, account);
+    navigateTo("#/profile");
+});
+
+// =================================
+//  Authentication State Management
+// =================================
+
+function setAuthState(isAuth, user = null){
+    currentUser = isAuth ? user : null;
+
+    if(isAuth)
+    {
+        document.body.classList.add("authenticated");
+        document.body.classList.remove("not-authenticated");
+        if(user.role === "admin"){
+            document.body.classList.add("is-admin");
+        }
+    }
+    else
+    {
+        document.body.classList.remove("authenticated", "is-admin");
+        document.body.classList.add("not-authenticated");
+    }
+}
+
+window.addEventListener("load", function(){
+    const email = localStorage.getItem("auth_token");
+    if(email)
+    {
+        const account = window.db.accounts.find(acc => acc.email === email);
+        if(account){
+            setAuthState(true, account);
+        }
+    }
+
+});
+
+// ================
+//     Logout
+// ================
+
+document.querySelector("a[href='#/logout']").forEach(btn => {
+    btn.addEventListener("click", function(e){
+        e.preventDefault();
+        localStorage.removeItem("auth_token");
+        setAuthState(false);
+        navigateTo("#/");
+    });
 });
